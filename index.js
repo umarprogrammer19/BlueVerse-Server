ba389ae2c05b1f86bcf9ffb2accc895bb227ac22
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 14:10:52 +0500

    cookie-parser

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ import { Welcome } from "./helpers/welcome.js";
 import "dotenv/config";
 import { connectDB } from "./database/connection.js";
 import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT;
