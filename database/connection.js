871867abf06043187aea829f1c6056fe820582b5
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 11:59:15 +0500

    Connection Successfull

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(process.env.MONGO_URI);
+        const connectionInstance = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
