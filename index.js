b471c0faea8b52b7cf21aebf9e3ce66ff0580fbf
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 14:09:57 +0500

    Index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import express from "express";
 import { Welcome } from "./helpers/welcome.js";
 import "dotenv/config";
 import { connectDB } from "./database/connection.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+
 const port = process.env.PORT;
 
 app.get("/", Welcome);
@@ -13,6 +18,6 @@ connectDB().then(() => {
         console.log(`Server is running on the port ${port}`)
     })
 }).catch((error) => {
-    console.error("Server failed to start:", error.message);
+    console.error("Server failed to start:", error.message || "Internal Server Error");
     process.exit(1);
 });
