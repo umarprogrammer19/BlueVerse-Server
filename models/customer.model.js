2e82dd50296343d865c3cb4af7b89e8ff0b6c554
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 13:28:53 +0500

    Customer Model

--- models/customer.model.js
+++ models/customer.model.js
@@ -3,20 +3,37 @@ import mongoose from "mongoose";
 
 const customerSchema = new mongoose.Schema(
     {
-        // kis user ka customer hai (optional)
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-
-        firstName: { type: String, required: true },
-        lastName: { type: String, required: true },
-        email: { type: String, required: true },
-        phoneNumber: { type: String, required: true },
-        licencePlateNumber: { type: String, required: true },
-        address: { type: String, required: true },
-
-        // tumhare customerDetails se:
-        state: { type: String }, // active / trial / cancelled, ya jo bhi
-    },
-    { timestamps: true }
-);
+        user: {
+            type: String,
+            required: true,
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        phoneNumber: {
+            type: String,
+            required: true
+        },
+        licencePlateNumber: {
+            type: String,
+            required: true
+        },
+        address: {
+            type: String,
+        },
+        state: {
+            type: String,
+            default: "Al Quoz"
+        },
+    }, { timestamps: true });
 
 export default mongoose.model("Customer", customerSchema);
