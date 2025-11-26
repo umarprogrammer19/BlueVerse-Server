b9653ef62312b592859b1d767480bd9996fc6508
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 13:10:04 +0500

    User models

---
+++ models/user.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    }
+});
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+export const User = mongoose.model("User", userSchema);
