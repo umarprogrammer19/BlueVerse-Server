8bf9bb2fd16d636fad65c14c1ed89b616fd6d3d3
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-10 15:36:22 +0500

    Updated Docs

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     origin: ["http://localhost:5173", "https://blueverse-checkout.netlify.app"],
-}))
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
